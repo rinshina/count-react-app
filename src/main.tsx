3dd6ce3e0787f85c5a025f0764cff49d3f3ab863
Rinshina Febin k <81473566+rinshina@users.noreply.github.com>
2025-02-03 02:24:31 +0530

    google SignIn component

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { ChakraProvider } from "@chakra-ui/react";
+import { defaultSystem } from "@chakra-ui/react";
+
+const CLIENT_ID =
+  "662314130285-3tb9gi75qnbl9rphu7vu5hfpu6fpe820.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <>
-      <App />
-    </>
+    <ChakraProvider value={defaultSystem}>
+      <GoogleOAuthProvider clientId={CLIENT_ID}>
+        <App />
+      </GoogleOAuthProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
