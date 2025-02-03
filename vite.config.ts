73205b78eeb05a753e29becb6fdd076dbb3b0d42
Rinshina Febin k <81473566+rinshina@users.noreply.github.com>
2025-02-03 12:15:18 +0530

    resolved mistakes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/count-react-app/', //  Ensures correct paths for GitHub Pages
+  base: '/count-react-app', //  Ensures correct paths for GitHub Pages
 });
